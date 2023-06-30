6bb7722c4dd59894491b85c7de15e4afbc80073f
UB PGI Gersi Muca <gersi.muca@planetgroupint.com>
2023-06-30 14:41:12 +0700

    Implemented service and created CI and scripts folders

--- src/main/java/com/gersimuca/servicemanagement/service/Service.java
+++ src/main/java/com/gersimuca/servicemanagement/service/Service.java
@@ -1,16 +1,50 @@
 package com.gersimuca.servicemanagement.service;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Service {
     @Id
+    @SequenceGenerator(
+            name = "service_id_sequence",
+            sequenceName = "service_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "service_id_sequence"
+    )
     private Long id;
 
     private String name;
 
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Service service = (Service) o;
+        return Objects.equals(id, service.id) && Objects.equals(name, service.name) && Objects.equals(description, service.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Service{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
